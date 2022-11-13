606e7d474f1165bb9217ceef7851808ee10843ec
Taylher <alexmota@alu.ufc.br>
2022-11-13 17:44:01 -0300

    criando meus primeiros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ButtonComponentComponent } from './components/button-component/button-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    ButtonComponentComponent
   ],
   imports: [
     BrowserModule
