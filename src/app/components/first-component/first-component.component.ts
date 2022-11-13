d3a2119cdbd592acc42c76a19cdceffdc6bace46
Taylher <alexmota@alu.ufc.br>
2022-11-13 18:19:12 -0300

    adicionando array de objects e exibindo no code

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
 export class FirstComponentComponent implements OnInit {
+  persons = [
+    {
+      name: 'Alex',
+      age: 20,
+      job: 'Developer',
+    },
+    { name: 'Eduardo', age: 22, job: 'Nutritionist' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
